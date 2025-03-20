c4806bba11ab8fb0f0c47717ceaa8dc48b6fd5ef
unknown <Shang,Zhendan>
2025-03-20 14:30:03 +0000

    first empty prototype

---
+++ real_estate_adviser/dashboards/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('search_results/', views.search_results, name='search_results'),
+    path('user_account/', views.user_account, name='user_account'),
+    
+]
--- real_estate_adviser/dashboards/views.py
+++ real_estate_adviser/dashboards/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.http import JsonResponse
+
+from .forms import SearchForm
+from .global_vars import *
+
+def index(request):
+    return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def search_results(request):
+    query = request.GET.get('search_str')
+    results = []
+    query = ""
+    return render(request, 'search_results.html', {'query': query, 'results': results})
+
+@login_required(login_url='/login/')
+def user_account(request):
+
+    return render(request, 'user_account.html')
